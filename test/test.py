cb8f4c122d1119badc05b44c3be4b0868ea72bdc
Austin Steady <asteady23@gmail.com>
2021-12-10 20:16:55 -0800

    add working but ugly test

---
+++ test/test.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append('../ftx_client')
+import ftx_client.api
+import os
+import datetime as dt
+import matplotlib.pyplot as plt
+import time
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+import dateutil
+
+def test_ticks_threaded():
+    client = ftx_client.api.HelperClient(key=os.environ['API_KEY'], secret=os.environ['API_SECRET'], platform='com')
+
+    start = dateutil.parser.parse('2021-12-01 00:00:00+00:00')
+    end = start + dt.timedelta(seconds=60*60)
+
+    df = client.get_historical_ticks_threaded('BTC-PERP', since=start, til=end)
+
+    # TODO ugh abs path
+    with open('/Users/austin/code/ftx_client/test/fixtures/ticks.csv', 'r') as f:
+        expected_df = pd.read_csv(f, index_col=0, parse_dates=True).rename(columns={'time.1': 'time'})
+        assert_frame_equal(df, expected_df)
+
+def test_prices_threaded():
+    client = ftx_client.api.HelperClient(key=os.environ['API_KEY'], secret=os.environ['API_SECRET'], platform='com')
+
+    start = dt.datetime.now() - dt.timedelta(days=5)
+    then1 = time.time()
+    df = client.get_prices('SOL-PERP', since_date=start, window_size_secs=60)
+    print(time.time() - then1)
+    print(len(df))
+
+test_ticks_threaded()
